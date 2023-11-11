4bdebec2d220c342f5fd905af7493fb3e34afffa
Alaister Young <a@alaisteryoung.com>
2023-11-12 00:53:13 +1100

    add platform colors

---
+++ platform-colors.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  colors: {
+    background: {
+      light: '#ffffff',
+      dark: '#000000',
+    },
+    accent: 'pink',
+  },
+  javascript: {
+    typescript: true,
+    outputDirectory: 'src/lib/colors/',
+  },
+  ios: {
+    outputDirectory: 'ios/fourthplace/Images.xcassets/',
+  },
+  android: {
+    outputDirectory: 'android/app/src/main/res/',
+  },
+}
---
+++ src/lib/colors/index.android.ts
@@ -0,0 +1,4 @@
+import { PlatformColor } from 'react-native'
+
+export const background = PlatformColor('@color/rnpc_background')
+export const accent = PlatformColor('@color/rnpc_accent')
